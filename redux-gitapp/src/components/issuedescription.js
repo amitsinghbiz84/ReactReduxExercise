515318fc8945d1221c20dc88e37b63bca1481fda
Amit Kumar <akumar12@sapient.com>
2018-08-21 12:12:07 +0530

    committed base files.

---
+++ redux-gitapp/src/components/issuedescription.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const IssueDescription = () => {
+  return (
+    <div>
+      <h1>  Issue Description </h1>
+    </div>
+  )
+}
+export default IssueDescription;
---
+++ redux-gitapp/src/components/issuelisting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const IssueListing =  () => {
+  return (
+    <div>
+      <h1>Issue Listing</h1>
+    </div>
+  )
+}
+export default IssueListing;
